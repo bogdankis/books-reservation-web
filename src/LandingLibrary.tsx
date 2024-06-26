f21fc672660f32e2bb20f712fce0157ac8c67786
bogdankis <chismbogdan@gmail.com>
2024-06-26 14:23:49 +0300

    updated landinglibrary component
    added mobile resposiveness to the component

--- src/LandingLibrary.tsx
+++ src/LandingLibrary.tsx
@@ -7,55 +7,42 @@ const LandingLibrary = () => {
 <div className="border-bottom shadow d-flex flex-column align-items-center"  style={{minHeight:'720px', padding: '100px'}}>
       <div className="mb-4">
         <h2 className="text-center fw-bold display-5">What does bookster do?</h2>
-        <div className="text-center">First modern platform which brings books closer to people.</div>
+        <div className="text-center fs-5">First modern platform which brings books closer to people.</div>
       </div>
-
-      <div>
-        <table>
-          <tbody>
-            <tr>
-              <td className="text-center py-2">
-                <img
+<div className="container text-center">
+ 
+      <div className="row">
+      <div className="col-lg-4">
+      <img
                   src={ModernImg}
                   alt="Campaign"
                   className="img-fluid"
                   style={{ width: '250px', height: '250px' }}
                 />
-              </td>
-              <td className="text-center">
-                <img
+                        <h2 className="fw-bolder">Various <br />libraries</h2>
+        <p className='fs-5 mt-4'>You have free access to the best books, articles, and videos.</p>
+      </div>
+      <div className="col-lg-4">
+      <img
                   src={PersonalizedImg}
                   alt="Modern"
                   className="img-fluid"
                   style={{ width: '250px', height: '250px' }}
-                />
-              </td>
-              <td className="text-center">
-                <img
+                />        <h2 className="fw-bolder">Personalized <br />recommendations</h2>
+        <p className='fs-5 mt-4'>You get personalized reading recommendations based on your preferences.</p>
+      </div>
+      <div className="col-lg-4">
+      <img
                   src={CampaingImg}
                   alt="Personalized"
                   className="img-fluid"
                   style={{ width: '250px', height: '250px' }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td className="col-md-4 text-center">
-                <h3 className="fw-bolder">Various <br />libraries</h3>
-                <div>You have free access to the best books, articles, and videos.</div>
-              </td>
-              <td className="col-md-4 text-center">
-                <h3 className="fw-bolder">Personalized <br />recommendations</h3>
-                <div>You get personalized reading recommendations based on your preferences.</div>
-              </td>
-              <td className="col-md-4 text-center">
-                <h3 className="fw-bolder">Campaign to <br />encourage reading</h3>
-                <div>We send you reviews of the best books and promote reading in the community of readers.</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+                />        <h2 className="fw-bolder">Campaign to <br />encourage reading</h2>
+        <p className='fs-5 mt-4'>We send you reviews of the best books and promote reading in the community of readers.</p>
       </div>
+      </div>
+      
+    </div>
     </div>
     );
 };
