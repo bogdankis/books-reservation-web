01c712331bd6f3cc9694b584c83a8f7ddf4eabeb
bogdankis <chismbogdan@gmail.com>
2024-06-28 16:30:22 +0300

    created benefits component
    created component and added content

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Landpage from './Landpage';
 import Footer from './Footer';
 import LandingLibrary from './LandingLibrary';
 import AnimatedSection from './AnimatedSection';
+import Benefits from './Benefits';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Landpage></Landpage>
       <LandingLibrary></LandingLibrary>
       <AnimatedSection></AnimatedSection>
+      <Benefits></Benefits>
       <Footer></Footer>
     </div>
   );
---
+++ src/Benefits.tsx
@@ -0,0 +1,48 @@
+
+
+const Benefits = () => {
+  return (
+    <div className="container mt-5">
+<div className="featurette-divider">
+
+<div className="row featurette">
+      <div className="col-md-7">
+        <h2 className="featurette-heading fw-normal lh-1">The best books.</h2>
+        <p className="lead">Thousands of personal development, professional, fiction or hobby titles.</p>
+      </div>
+      <div className="col-md-5">
+      </div>
+</div>
+
+<div className="featurette-divider">
+
+    <div className="row featurette">
+      <div className="col-md-7 order-md-2">
+        <h2 className="featurette-heading fw-normal lh-1">Articles for your inspiration.</h2>
+        <p className="lead">Unlimited access to a vast collection of award-winning articles written by renowned authors.</p>
+      </div>
+      <div className="col-md-5 order-md-1">
+      </div>
+    </div>
+</div>
+    <div className="featurette-divider">
+
+    <div className="row featurette">
+      <div className="col-md-7">
+        <h2 className="featurette-heading fw-normal lh-1">Exciting videos</h2>
+        <p className="lead">The best TED presentations or courses from top universities.</p>
+      </div>
+      <div className="col-md-5">
+      </div>
+    </div>
+</div>
+</div>
+</div>
+)
+  
+}
+
+export default Benefits
+
+
+
