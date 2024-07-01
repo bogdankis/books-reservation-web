421f5c3e4067317be7f3220546a8e8ba6656580c
bogdankis <chismbogdan@gmail.com>
2024-07-01 14:35:00 +0300

    updated component benefits
    added images and postion them

--- src/Benefits.tsx
+++ src/Benefits.tsx
@@ -1,38 +1,45 @@
 
+import imgBooks from './images/img-books.svg';
+import imgArticle from './images/img-article.png';
+import imgVideo from './images/img-video.png';
+
 
 const Benefits = () => {
   return (
-    <div className="container mt-5">
-<div className="featurette-divider">
+    <div className="container mt-2">
+<div className="featurette-divider text-center ">
 
-<div className="row featurette">
-      <div className="col-md-7">
-        <h2 className="featurette-heading fw-normal lh-1">The best books.</h2>
-        <p className="lead">Thousands of personal development, professional, fiction or hobby titles.</p>
+<div className="row featurette pt-5">
+      <div className="col-md-7 align-items-center order-first order-md-1">
+        <h2 className="featurette-heading fw-normal lh-1 fw-bold">The best books.</h2>
+        <p className="lead fw-normal">Thousands of personal development, <br /> professional, fiction or hobby titles.</p>
       </div>
       <div className="col-md-5">
+      <img src={imgBooks} alt="books" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500"/>
       </div>
 </div>
 
-<div className="featurette-divider">
+<div className="featurette-divider mt-5">
 
     <div className="row featurette">
-      <div className="col-md-7 order-md-2">
-        <h2 className="featurette-heading fw-normal lh-1">Articles for your inspiration.</h2>
-        <p className="lead">Unlimited access to a vast collection of award-winning articles written by renowned authors.</p>
+      <div className="col-md-7 align-items-center order-first order-md-1">
+        <h2 className="featurette-heading fw-normal lh-1 fw-bold">Articles for your inspiration.</h2>
+        <p className="lead fw-normal">Unlimited access to a vast collection of <br />award-winning articles written by renowned authors.</p>
       </div>
-      <div className="col-md-5 order-md-1">
+      <div className="col-md-5 order-md-2">
+      <img src={imgArticle} alt="article" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500"/>
       </div>
     </div>
 </div>
-    <div className="featurette-divider">
+    <div className="featurette-divider mt-5">
 
     <div className="row featurette">
-      <div className="col-md-7">
-        <h2 className="featurette-heading fw-normal lh-1">Exciting videos</h2>
-        <p className="lead">The best TED presentations or courses from top universities.</p>
+      <div className="col-md-7 align-items-center order-first order-md-1">
+        <h2 className="featurette-heading fw-normal lh-1 fw-bold">Exciting videos</h2>
+        <p className="lead fw-normal">The best TED presentations or courses from top universities.</p>
       </div>
       <div className="col-md-5">
+      <img src={imgVideo} alt="video" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500"/>
       </div>
     </div>
 </div>
